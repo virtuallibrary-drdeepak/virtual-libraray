c273fd278d53ec8ff181002d4a8aeff1443a68eb
Mac OS <macos@MacBook-Air.local>
2025-12-25 00:00:23 +0530

    fix: facebook event manager added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,24 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+
+        {/* Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1788102748497505');
+fbq('track', 'PageView');`,
+          }}
+        />
+        {/* End Facebook Pixel Code */}
       </Head>
       <body>
         {/* Google Tag Manager (noscript) */}
@@ -27,6 +45,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+
+        {/* Facebook Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1788102748497505&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Facebook Pixel (noscript) */}
+
         <Main />
         <NextScript />
       </body>
